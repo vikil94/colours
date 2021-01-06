a5bed5a2749e7dca4a18ca8931c8ad5f50b2c51b
Vikil Naik <vikilnaik@Vikils-MBP.hitronhub.home>
2021-01-05 21:07:05 -0500

    Copy popup animation has been added so when the user clicks on the hex code the animation shows up that it has been copied to the clipboard - will now program it so that the hex code is stored

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const colourDivs = document.querySelectorAll(".colour");
 const generateBtn = document.querySelector(".generate");
 const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexes = document.querySelectorAll(".colour h2");
+const popup = document.querySelector(".copy-container");
 let initialColours;
 
 // Event Listeners
@@ -15,6 +16,18 @@ colourDivs.forEach((div, index) => {
 	div.addEventListener("change", () => {
 		updateTextUI(index);
 	});
+});
+
+currentHexes.forEach((hex) => {
+	hex.addEventListener("click", () => {
+		copyToClipboard(hex);
+	});
+});
+
+popup.addEventListener("transitionend", () => {
+	const popupBox = popup.children[0];
+	popup.classList.remove("active");
+	popupBox.classList.remove("active");
 });
 
 //Functions
@@ -144,6 +157,19 @@ function resetInputs() {
 			slider.value = Math.floor(satValue * 100) / 100;
 		}
 	});
+}
+
+function copyToClipboard(hex) {
+	const el = document.createElement("textarea");
+	el.value = hex.innerText;
+	document.body.appendChild(el);
+	el.select();
+	document.execCommand("copy");
+	document.body.removeChild(el);
+	// Pop up animation
+	const popupBox = popup.children[0];
+	popup.classList.add("active");
+	popupBox.classList.add("active");
 }
 
 randomColours();
