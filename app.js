45b48ac8e3bad6a6a83ae51c94f3f996247d9b26
Vikil Naik <vikilnaik@Vikils-MBP.hitronhub.home>
2021-01-07 17:00:52 -0500

    The user can now save their palettes which will dynamically update in the libray panel - once in the library panel the user is able to select their palettes and the apps colours will change back to that specific palette

--- app.js
+++ app.js
@@ -235,12 +235,17 @@ const submitSave = document.querySelector(".submit-save");
 const closeSave = document.querySelector(".close-save");
 const saveContainer = document.querySelector(".save-container");
 const saveInput = document.querySelector(".save-container input");
+const libraryContainer = document.querySelector(".library-container");
+const libraryBtn = document.querySelector(".library");
+const closeLibraryBtn = document.querySelector(".close-library");
 
 // Event listeners
 
 saveBtn.addEventListener("click", openPalette);
 closeSave.addEventListener("click", closePalette);
 submitSave.addEventListener("click", savePalette);
+libraryBtn.addEventListener("click", openLibrary);
+closeLibraryBtn.addEventListener("click", closeLibrary);
 
 function openPalette(e) {
 	const popup = saveContainer.children[0];
@@ -269,6 +274,42 @@ function savePalette(e) {
 	// Save to local storage
 	saveToLocal(paletteObj);
 	saveInput.value = "";
+
+	// Generate palette for the library
+	const palette = document.createElement("div");
+	palette.classList.add("custom-palette");
+	const title = document.createElement("h4");
+	title.innerText = paletteObj.name;
+	const preview = document.createElement("div");
+	preview.classList.add("small-preview");
+	paletteObj.colours.forEach((smallColour) => {
+		const smallDiv = document.createElement("div");
+		smallDiv.style.backgroundColor = smallColour;
+		preview.appendChild(smallDiv);
+	});
+
+	const paletteBtn = document.createElement("button");
+	paletteBtn.classList.add("pick-palette-btn");
+	paletteBtn.classList.add(paletteObj.nr);
+	paletteBtn.innerText = "Select";
+
+	// Attach event to 'select' button
+	paletteBtn.addEventListener("click", (e) => {
+		closeLibrary();
+		const paletteIndex = e.target.classList[1];
+		initialColours = [];
+		savedPalettes[paletteIndex].colours.forEach((colour, index) => {
+			initialColours.push(colour);
+			colourDivs[index].style.backgroundColor = colour;
+			const text = colourDivs[index].children[0];
+		});
+	});
+
+	// Append to library
+	palette.appendChild(title);
+	palette.appendChild(preview);
+	palette.appendChild(paletteBtn);
+	libraryContainer.children[0].appendChild(palette);
 }
 
 function saveToLocal(paletteObj) {
@@ -281,6 +322,18 @@ function saveToLocal(paletteObj) {
 
 	localPalettes.push(paletteObj);
 	localStorage.setItem("palettes", JSON.stringify(localPalettes));
+}
+
+function openLibrary() {
+	const popup = libraryContainer.children[0];
+	libraryContainer.classList.add("active");
+	popup.classList.add("active");
+}
+
+function closeLibrary() {
+	const popup = libraryContainer.children[0];
+	libraryContainer.classList.remove("active");
+	popup.classList.remove("active");
 }
 
 randomColours();
