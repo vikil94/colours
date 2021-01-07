693796a64fe676e5bfa2d645a67ff7c5ef23112d
Vikil Naik <vikilnaik@Vikils-MBP.hitronhub.home>
2021-01-07 17:37:22 -0500

    Updated so that when the user selects one of their saved palettes the sliders, contrast, and hex code text changes to the correct settings - before it would stay the same as the previous palettes - it has been updated properly now

--- app.js
+++ app.js
@@ -302,7 +302,10 @@ function savePalette(e) {
 			initialColours.push(colour);
 			colourDivs[index].style.backgroundColor = colour;
 			const text = colourDivs[index].children[0];
+			checkTextContrast(colour, text);
+			updateTextUI(index);
 		});
+		resetInputs();
 	});
 
 	// Append to library
@@ -336,4 +339,55 @@ function closeLibrary() {
 	popup.classList.remove("active");
 }
 
+function getLocal() {
+	if (localStorage.getItem("palettes") === null) {
+		localPalettes = [];
+	} else {
+		const paletteObjects = JSON.parse(localStorage.getItem("palettes"));
+		paletteObjects.forEach((paletteObj) => {
+			// Generate palette for the library
+			const palette = document.createElement("div");
+			palette.classList.add("custom-palette");
+			const title = document.createElement("h4");
+			title.innerText = paletteObj.name;
+			const preview = document.createElement("div");
+			preview.classList.add("small-preview");
+			paletteObj.colours.forEach((smallColour) => {
+				const smallDiv = document.createElement("div");
+				smallDiv.style.backgroundColor = smallColour;
+				preview.appendChild(smallDiv);
+			});
+
+			const paletteBtn = document.createElement("button");
+			paletteBtn.classList.add("pick-palette-btn");
+			paletteBtn.classList.add(paletteObj.nr);
+			paletteBtn.innerText = "Select";
+
+			// Attach event to 'select' button
+			paletteBtn.addEventListener("click", (e) => {
+				closeLibrary();
+				const paletteIndex = e.target.classList[1];
+				initialColours = [];
+				paletteObjects[paletteIndex].colours.forEach(
+					(colour, index) => {
+						initialColours.push(colour);
+						colourDivs[index].style.backgroundColor = colour;
+						const text = colourDivs[index].children[0];
+						checkTextContrast(colour, text);
+						updateTextUI(index);
+					}
+				);
+				resetInputs();
+			});
+
+			// Append to library
+			palette.appendChild(title);
+			palette.appendChild(preview);
+			palette.appendChild(paletteBtn);
+			libraryContainer.children[0].appendChild(palette);
+		});
+	}
+}
+
+getLocal();
 randomColours();
